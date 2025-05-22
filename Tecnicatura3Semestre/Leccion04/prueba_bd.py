6be7c0741914a65e5d15ceaee92176138c2395c7
AugustoVernengoLima <augustojvl8@gmail.com>
2025-05-22 19:17:17 -0300

    cambios de clase 6

--- Tecnicatura3Semestre/Leccion04/prueba_bd.py
+++ Tecnicatura3Semestre/Leccion04/prueba_bd.py
@@ -1,14 +1,13 @@
-import psycopg2 # esto es para poder conectar a Postgre
+import psycopg2# esto es para poder conectar a Postgre
 
 conexion = psycopg2.connect(user="postgres", password="admin", host="127.0.0.1", port="5432", database="test_bd")
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            cursor = conexion.cursor()
             sentencia = "SELECT * FROM persona WHERE id_persona = %s"
-            id_persona = input("Digite un numero para el id_persona:")
-            cursor.execute(sentencia, (id_persona,)) # de set ejecutamos la sentencia
-            registros = cursor.fetchone() #recuperamos todos los registros que seran una lista 
+            id_persona = input("Digite un numero para el id_persona: ")
+            cursor.execute(sentencia, (id_persona,))
+            registros = cursor.fetchone()
             print(registros)
 except Exception as e:
     print(f"Ocurrio un error{e}")
