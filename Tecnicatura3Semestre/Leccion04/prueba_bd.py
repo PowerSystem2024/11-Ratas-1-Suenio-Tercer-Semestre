bd0d49df5461512e41a74c310a7169df498f9e7b
AugustoVernengoLima <augustojvl8@gmail.com>
2025-05-22 17:00:14 -0300

    cambios de los trabajos de python lines

--- Tecnicatura3Semestre/Leccion04/prueba_bd.py
+++ Tecnicatura3Semestre/Leccion04/prueba_bd.py
@@ -1,18 +1,15 @@
 import psycopg2 # esto es para poder conectar a Postgre
 
-conexion = psycopg2.connect(
-    user="postgres",
-    password="admin",
-    host="127.0.0.1",
-    port="5432",
-    database="test_bd"
-)
-
-cursor = conexion.cursor()
-sentencia = "SELECT * FROM persona"
-cursor.execute(sentencia) # de set ejecutamos la sentencia
-registros = cursor.fetchall() #recuperamos todos los registros que seran una lista 
-print(registros)
-
-cursor.close()
-conexion.close()
+conexion = psycopg2.connect(user="postgres", password="admin", host="127.0.0.1", port="5432", database="test_bd")
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            cursor = conexion.cursor()
+            sentencia = "SELECT * FROM persona"
+            cursor.execute(sentencia) # de set ejecutamos la sentencia
+            registros = cursor.fetchall() #recuperamos todos los registros que seran una lista 
+            print(registros)
+except Exception as e:
+    print(f"Ocurrio un error{e}")
+finally:        
+    conexion.close()
