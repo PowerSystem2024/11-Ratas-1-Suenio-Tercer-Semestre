00d85d3c11c6db6aca65014a22077d91f7812a1e
Nacho0150 <ignacioibanez01@gmail.com>
2025-05-22 21:14:14 -0300

    Agregando Clases 02 y 03 Java

---
+++ Tecnicatura3Semestre/Java/Leccion03/ModificadoresAcceso/src/main/java/paquete1/Clase2.java
@@ -0,0 +1,13 @@
+package paquete1;
+
+public class Clase2 {
+    String atributoDefault = "Valor del atributo default";
+
+    public Clase2() {
+        System.out.println("Constructor Default");
+    }
+
+    void metodoDefault() {
+        System.out.println("Método Default");
+    }
+}
